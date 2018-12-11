7274de788c9fa3c4162323a2ec9adafbed75b5b6
casonhawley <cason.hawley@gmail.com>
2018-12-11 16:08:30 -0700

    pre git hub commit

---
+++ react_frontend/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Movie Micro Service</h1>
+        
+      </div>
+    );
+  }
+}
